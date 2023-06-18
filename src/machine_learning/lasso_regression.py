e3a9b47fc14a9f5c335418d31a7528730ab0e74b
JhaNishant <nishantjha98@yahoo.in>
2023-06-18 22:57:57 +0200

    Updating Lasso

--- src/machine_learning/lasso_regression.py
+++ src/machine_learning/lasso_regression.py
@@ -1,13 +1,15 @@
 import argparse
 import logging
 
+import cudf
 import cupy as cp
 import joblib
 import matplotlib.pyplot as plt
 import pandas as pd
-from cuml.linear_model import Lasso
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
+from cuml import Lasso as cuLasso
+from cuml.metrics.regression import (mean_absolute_error, mean_squared_error,
+                                     r2_score)
+from cuml.model_selection import train_test_split as cuml_train_test_split
 
 
 def main():
@@ -63,16 +65,16 @@ def main():
     X = data.drop(target_column, axis=1)
     y = data[target_column]
 
-    # Convert dataframes to CuPy arrays for GPU computation
-    X = cp.asarray(X)
-    y = cp.asarray(y)
+    # Convert pandas dataframes to cuDF dataframes for GPU computation
+    X = cudf.DataFrame.from_pandas(X)
+    y = cudf.Series(y)
 
     # Split data into training and test sets
-    X_train, X_test, y_train, y_test = train_test_split(
+    X_train, X_test, y_train, y_test = cuml_train_test_split(
         X, y, test_size=0.2, random_state=42)
 
     # Initialize Lasso regression model
-    lasso = Lasso(alpha=0.1)
+    lasso = cuLasso(alpha=0.1)
 
     # Train model
     lasso.fit(X_train, y_train)
@@ -80,6 +82,9 @@ def main():
 
     # Predict on test data
     y_pred = lasso.predict(X_test)
+
+    # Convert cuDF series to cupy array for further computation
+    y_pred = cp.asarray(y_pred)
 
     # Calculate Mean Squared Error
     mse = mean_squared_error(y_test, y_pred)
@@ -90,15 +95,32 @@ def main():
     logging.info(f'Model saved to {model_file}.')
 
     # Save coefficients
-    coeff_df = pd.DataFrame(lasso.coef_, X.columns, columns=['Coefficient'])
+    coeff_df = cudf.DataFrame(
+        {'Coefficient': lasso.coef_}, index=X.columns).to_pandas()
     coeff_df.to_csv(coeff_file)
     logging.info(f'Coefficients saved to {coeff_file}.')
 
+    # Convert cuDF series to cupy array for plotting
+    y_test = cp.asarray(y_test)
+
     # Plot actual vs predicted values
-    plt.scatter(y_test.get(), y_pred)
+    plt.scatter(y_test.get(), y_pred.get())
     plt.xlabel('Actual Values')
     plt.ylabel('Predicted Values')
     plt.title('Actual vs Predicted Values for Lasso Regression')
+
+    # Calculate and display regression metrics
+    mae = mean_absolute_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    plt.text(
+        0.05,
+        0.85,
+        f'MAE: {mae:.2f}\nMSE: {mse:.2f}\nR2: {r2:.2f}',
+        transform=plt.gca().transAxes,
+        bbox=dict(
+            facecolor='white',
+            alpha=0.5))
+
     plt.savefig(plot_file)
     logging.info(f'Plot saved as {plot_file}.')
 
