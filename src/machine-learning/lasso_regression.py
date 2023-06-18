47fd21b41d704bbec7fcdfb49a0391bee098c710
Nishant Jha <nishantjha98@yahoo.in>
2023-06-18 18:34:47 +0530

    Adding machine-learning

---
+++ src/machine-learning/lasso_regression.py
@@ -0,0 +1,90 @@
+import argparse
+import logging
+
+import cupy as cp
+import joblib
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import Lasso
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+
+def main():
+    # Set up logging
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s')
+
+    logging.info('Starting script...')
+
+    # Argument parser for command line interaction
+    parser = argparse.ArgumentParser(
+        description="Train a Lasso regression model."
+    )
+    # Input dataset file path
+    parser.add_argument(
+        "input_file",
+        type=str,
+        help="Path to the input dataset file."
+    )
+    # Output model file path
+    parser.add_argument(
+        "model_file",
+        type=str,
+        help="Path to the output file for the trained model."
+    )
+
+    args = parser.parse_args()
+
+    input_file = args.input_file
+    model_file = args.model_file
+
+    # Load dataset
+    data = pd.read_csv(input_file)
+
+    target_column = 'soc_percent'
+
+    # Split data into features and target
+    X = data.drop(target_column, axis=1)
+    y = data[target_column]
+
+    # Convert dataframes to CuPy arrays for GPU computation
+    X = cp.asarray(X)
+    y = cp.asarray(y)
+
+    # Split data into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42)
+
+    # Initialize Lasso regression model
+    lasso = Lasso(alpha=0.1)
+
+    # Train model
+    lasso.fit(X_train, y_train)
+    logging.info('Model trained.')
+
+    # Predict on test data
+    y_pred = lasso.predict(X_test)
+
+    # Calculate Mean Squared Error
+    mse = mean_squared_error(y_test, y_pred)
+    logging.info(f'Mean Squared Error: {mse}')
+
+    # Save trained model
+    joblib.dump(lasso, model_file)
+    logging.info(f'Model saved to {model_file}.')
+
+    # Plot actual vs predicted values
+    plt.scatter(y_test.get(), y_pred)
+    plt.xlabel('Actual Values')
+    plt.ylabel('Predicted Values')
+    plt.title('Actual vs Predicted Values for Lasso Regression')
+    plt.savefig('lasso_regression_plot.png')
+    logging.info('Plot saved as lasso_regression_plot.png')
+
+    logging.info('Finished script.')
+
+
+if __name__ == '__main__':
+    main()
